09a408e3595fa2401f749e9650fe47065bd62fab
Guillermo Lòpez Garcìa <willy@MacBook-Pro-de-Guillermo.local>
2020-10-05 15:28:18 -0500

    Updates

---
+++ precache-manifest.c7a9482da9867fabc7f60c61b8aa19d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f270ef1dcb1211be2ab3f6fa129f0f",
+    "url": "/bs5/index.html"
+  },
+  {
+    "revision": "205c36446be39bb0a893",
+    "url": "/bs5/static/css/main.27d3e904.chunk.css"
+  },
+  {
+    "revision": "55e14774b0a8be2b3358",
+    "url": "/bs5/static/js/2.af0cf8c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bs5/static/js/2.af0cf8c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "205c36446be39bb0a893",
+    "url": "/bs5/static/js/main.dbe2ec4c.chunk.js"
+  },
+  {
+    "revision": "7b821b9b62ad00d306e0",
+    "url": "/bs5/static/js/runtime-main.e6ef84ab.js"
+  }
+]);
