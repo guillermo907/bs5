1cc1a042a6a2383dacc567a432f3a0aec9782bf2
Guillermo Lòpez Garcìa <willy@MacBook-Pro-de-Guillermo.local>
2020-10-09 17:47:59 -0500

    cambios diseño

--- src/components/ProductBanner.js
+++ src/components/ProductBanner.js
@@ -6,8 +6,8 @@ const ProductBanner = () =>{
             <h1>Producto</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
             <p>$48,999.00</p>
-            <button className="btn btn-dark m-2">Comprar</button> <br/>    
-            <img className="p-2 rounded" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR7L_ckBEP8hzkChkkAlM-tb76uo7A0ryGzbg&usqp=CAU" /> <br/>
+            <button className="btn btn-outline-dark m-2">Comprar</button> <br/>    
+            <img className="p-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR7L_ckBEP8hzkChkkAlM-tb76uo7A0ryGzbg&usqp=CAU" /> <br/>
             <a>Mas información</a> <br/>
         </div>
     );
