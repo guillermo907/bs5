6665cc545d1c7c98bc21f63e99f26aea31a6c13b
Guillermo Lòpez Garcìa <willy@MacBook-Pro-de-Guillermo.local>
2020-10-09 18:54:28 -0500

    cambios

--- src/components/About.js
+++ src/components/About.js
@@ -4,14 +4,12 @@ const About = () => {
 
     return (
         <div className="container-fluid text-white p-2 aboutContainer align-items-center text-center justify-content-center">
-            <div className="row">
-                <div className="col-sm">
-                    asdasd
-                </div>
-                <div className="col-sm">
-                    sdasdsad
-                </div>
-            </div>
+            <h1>
+                <svg width="15em" height="15em" viewBox="0 0 16 16" className="bi bi-book-half p-4" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M8.5 2.687v9.746c.935-.53 2.12-.603 3.213-.493 1.18.12 2.37.461 3.287.811V2.828c-.885-.37-2.154-.769-3.388-.893-1.33-.134-2.458.063-3.112.752zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+                </svg>
+            </h1>
+            <h1>Estudia Hijo de tu puta madre</h1>
         </div>
     );
 }
